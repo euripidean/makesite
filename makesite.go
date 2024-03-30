bdcd1d605aae6b88c29401add125dc4686c2cb2c
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-03-30 16:09:25 -0700

    Completed generating first-post.html

--- makesite.go
+++ makesite.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"os"
+)
+
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content	  string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileContents, err := os.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+}
+
+page := Page{
+	TextFilePath: "first-post.txt", 
+	TextFileName: "first-post.txt", 
+	HTMLPagePath: "first-post.html", 
+	Content: string(fileContents),
+}
+
+t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+
+newFile, err := os.Create(page.HTMLPagePath)
+if err != nil {
+	panic(err)
+}
+
+t.Execute(newFile, page)
+
 }
