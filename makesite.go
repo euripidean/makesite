1c002314c07ab4b7ed0ea3065e4714d23f6a7b5f
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-04-01 16:23:56 -0700

    Reading files from directory

--- makesite.go
+++ makesite.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -16,11 +17,31 @@ type Page struct {
 }
 
 func main() {
-	fileName := flag.String("file", "first-post.txt", "The name of the file to convert to HTML")
+	// fileName := flag.String("file", "first-post.txt", "The name of the file to convert to HTML")
+	dir := flag.String("dir", ".", "The directory where the text file is located")
 	flag.Parse()
-	TextFileName := strings.TrimSuffix(*fileName, ".txt")
+	
 
-page := Page{
+	// find all the text files in the directory
+	err := filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if strings.HasSuffix(info.Name(), ".txt") {
+            createHTMLPage(strings.TrimSuffix(info.Name(), ".txt"))
+        }
+
+        return nil
+    })
+
+    if err != nil {
+        panic(err)
+    }
+}
+
+func createHTMLPage(TextFileName string) {
+	page := Page{
 	TextFilePath: fmt.Sprintf("%s.txt", TextFileName),
 	HTMLPagePath: fmt.Sprintf("%s.html", TextFileName), 
 	Content: "",
@@ -35,12 +56,10 @@ page.Content = string(fileContents)
 
 t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
-
 newFile, err := os.Create(page.HTMLPagePath)
 if err != nil {
 	panic(err)
 }
 
 t.Execute(newFile, page)
-
 }
